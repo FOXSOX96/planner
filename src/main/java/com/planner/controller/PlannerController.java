977ce49556f63778d99a05141346e62202752e01
WonTaeJun <dnjsxonws94@gmail.com>
2025-11-03 14:50:25 +0900

    Chore : JPA 기본틀 패키지 생성2
    private final PlannerService plannerService; 문구 추가

--- src/main/java/com/planner/controller/PlannerController.java
+++ src/main/java/com/planner/controller/PlannerController.java
@@ -1,9 +1,11 @@
 package com.planner.controller;
 
+import com.planner.service.PlannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class PlannerController {
+    private final PlannerService plannerService;
 }
